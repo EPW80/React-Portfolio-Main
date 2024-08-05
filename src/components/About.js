214f30e3e757dc3d674af83544d36fcb45893ae4
EWP80 <erikpw009@gmail.com>
2024-08-05 16:56:38 -0700

    update to src/About

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,15 @@ function About() {
         src={process.env.PUBLIC_URL + "/img/erik.JPEG"}
         alt="Erik Parra Williams"
       />
-      {}
+      {/* <p className="content is-italic mt-4">
+        Full-Stack web developer leveraging a background in business to provide unique 
+        perspectives on how end-users interact with websites and software platforms—
+        earned a Full Stack Web Development certificate from the University of 
+        California Los Angeles Coding Boot Camp. Strengths in creativity, teamwork, 
+        and building projects from ideation to execution. 
+        Currently, a Senior at California State University Fullerton, completing a 
+        Bachelor of Science in Computer Science.
+      </p> */}
     </div>
   );
 }
